8abce549797b15e031e9888e7f7415a5dc150a0b
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-11-20 14:13:38 -0700

    final test2

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.get("/", async (req, res) => {
-  res.json("hello world for final test");
+  res.json("hello world for final test2");
 });
 
 app.listen(port, () => {

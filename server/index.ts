5e86d528f0110c697a3dec309b54cb99e6c6d045
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-11-20 13:37:58 -0700

    changed index.ts

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.get("/", async (req, res) => {
-  res.json("hello world");
+  res.json("hello world from the deployed site");
 });
 
 app.listen(port, () => {

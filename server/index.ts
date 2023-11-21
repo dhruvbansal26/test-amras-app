7270b5d2f80737d922cb67aaa926c63e4c25dcdf
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-11-21 12:39:44 -0700

    served

--- server/index.ts
+++ server/index.ts
@@ -10,9 +10,9 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
-app.get("/", async (req, res) => {
-  res.json("hello world for final test2");
-});
+// app.get("/", async (req, res) => {
+//   res.json("hello world for final test2");
+// });
 app.use(express.static("public"));
 app.use("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));

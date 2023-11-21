149bba8164ec7b05ddfd374c32440e6ec79e6be7
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-11-21 12:32:36 -0700

    changes

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,6 +1,3 @@
-import { Button, AppBar, Typography, Toolbar } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-
 export const Landing = () => {
   return <>Hi, this is the landing page!</>;
 };

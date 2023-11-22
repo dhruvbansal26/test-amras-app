2f4c0f91190e60393e427ae21c9af79093275627
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-11-22 16:39:19 -0700

    changes to package.json in server

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,13 +22,9 @@ const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/auth", auth_1.default);
-// app.get("/", async (req, res) => {
-//   res.json("hello world for final test2");
-// });
-// app.use(express.static("public"));
-// app.use("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.json("hello world for final test2");
+}));
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -10,13 +10,9 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
-// app.get("/", async (req, res) => {
-//   res.json("hello world for final test2");
-// });
-// app.use(express.static("public"));
-// app.use("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
+app.get("/", async (req, res) => {
+  res.json("hello world for final test2");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
